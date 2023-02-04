74bbf92c46385daefbb0c4bbec4528e957778fce
Benjamin Probst <e11907077@student.tuwien.ac.at>
2023-02-04 13:43:34 +0100

    -

--- ex3/tester.py
+++ ex3/tester.py
@@ -1,20 +1,41 @@
+from functools import partial
+
 from state import State
 from vizualizer import Visualizer
+from game import Game
+import threading
 import numpy as np
 import time
 
 
-map = np.ones(shape=(50,50))
-map[:,4] = 0
-map[:,5] = 0
-map[49,4] = 2
-map[49,5] = 2
-map[0,4] = 3
-map[0,5] = 3
+def please(state):
+    vis = Visualizer(state)
+
+
+map = np.ones(shape=(50, 50))
+map[:, 4] = 0
+map[:, 5] = 0
+map[49, 4] = 2
+map[49, 5] = 2
+map[0, 4] = 3
+map[0, 5] = 3
 print(map)
 
-state = State(map, (7,4))
-viz = Visualizer(state)
+# game = Game(map)
+# game.play_user()
 
+# print("hallo")
+
+state = State(map, (7, 4))
+
+
+
+t = threading.Thread(target=partial(please, state))
+t.start()
+
+print("hallo")
 time.sleep(5)
-# viz.updateAgent((7,1))
+state.setAgent((0, 0))
+time.sleep(5)
+state.setAgent((1, 1))
+print("hallo")
