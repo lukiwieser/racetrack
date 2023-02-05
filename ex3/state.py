52ad592ee73f89198f4fd978b66979575a78ca08
Benjamin Probst <e11907077@student.tuwien.ac.at>
2023-02-05 14:03:05 +0100

    added method setAgent

--- ex3/state.py
+++ ex3/state.py
@@ -4,3 +4,6 @@ class State:
     def __init__(self, racetrack: np.ndarray, agent_position: tuple[int,int]):
         self.racetrack = racetrack
         self.agent_position = agent_position
+
+    def setAgent(self, new_pos: tuple[int,int]):
+        self.agent_position = new_pos
