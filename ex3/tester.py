0c8fc7eb7b10691b2e4f32ee2a55087d3d20201e
Benjamin Probst <e11907077@student.tuwien.ac.at>
2023-02-05 15:35:18 +0100

    -

--- ex3/tester.py
+++ ex3/tester.py
@@ -1,5 +1,5 @@
 from functools import partial
-
+from display import Display
 from state import State
 from vizualizer import Visualizer
 from game import Game
@@ -8,34 +8,44 @@ import numpy as np
 import time
 
 
-def please(state):
-    vis = Visualizer(state)
+# def please(state):
+#     vis = Visualizer(state)
+#
 
-
-map = np.ones(shape=(50, 50))
-map[:, 4] = 0
-map[:, 5] = 0
+map = np.zeros(shape=(50, 50))
+map[:, 4] = 1
+map[:, 5] = 1
 map[49, 4] = 2
 map[49, 5] = 2
 map[0, 4] = 3
 map[0, 5] = 3
 print(map)
 
-# game = Game(map)
-# game.play_user()
+game = Game(map)
+game.play_user()
 
 # print("hallo")
 
-state = State(map, (7, 4))
+# state = State(map, (7, 4))
 
 
+# display = Display(state)
+#
+# print("hallo")
+# time.sleep(5)
+# display.update_agent((0,0))
+# print("hallo")
+# time.sleep(5)
+# display.update_agent((1,1))
+# print("hallo")
 
-t = threading.Thread(target=partial(please, state))
-t.start()
 
-print("hallo")
-time.sleep(5)
-state.setAgent((0, 0))
-time.sleep(5)
-state.setAgent((1, 1))
-print("hallo")
+# t = threading.Thread(target=partial(please, state))
+# t.start()
+#
+# print("hallo")
+# time.sleep(5)
+# state.setAgent((0, 0))
+# time.sleep(5)
+# state.setAgent((1, 1))
+# print("hallo")
