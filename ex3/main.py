87688285deff56325c6ba8dd72587019a61529b1
Benjamin Probst <benjamin.probst@gmx.at>
2023-02-22 13:29:59 +0100

    adjusted map and prints

--- ex3/main.py
+++ ex3/main.py
@@ -10,12 +10,33 @@ import random
 
 def play_user():
     map = np.zeros(shape=(50, 50))
-    map[:, 4] = 1
-    map[:, 5] = 1
-    map[49, 4] = 3
-    map[49, 5] = 3
-    map[0, 4] = 2
-    map[0, 5] = 2
+    map[:35, 24] = 1
+    map[:35, 25] = 1
+    map[:35, 26] = 1
+    map[:35, 27] = 1
+    map[:35, 28] = 1
+    map[:35, 29] = 1
+
+    map[29, 1:24] = 1
+    map[30, 1:24] = 1
+    map[31, 1:24] = 1
+    map[32, 1:24] = 1
+    map[33, 1:24] = 1
+    map[34, 1:24] = 1
+
+    map[29, 0] = 3
+    map[30, 0] = 3
+    map[31, 0] = 3
+    map[32, 0] = 3
+    map[33, 0] = 3
+    map[34, 0] = 3
+
+    map[0, 24] = 2
+    map[0, 25] = 2
+    map[0, 26] = 2
+    map[0, 27] = 2
+    map[0, 28] = 2
+    map[0, 29] = 2
 
     g = Game(racetrack=map, visualize=True)
 
@@ -28,18 +49,59 @@ def play_user():
     print("You reached the finish line!")
 
 def train_ai():
+    # map = np.zeros(shape=(50, 50))
+    # map[:, 4] = 1
+    # map[:, 5] = 1
+    # map[:, 6] = 1
+    # map[:, 7] = 1
+    # map[:, 8] = 1
+    # map[:, 9] = 1
+    # map[49, 4] = 3
+    # map[49, 5] = 3
+    # map[49, 6] = 3
+    # map[49, 7] = 3
+    # map[49, 8] = 3
+    # map[49, 9] = 3
+    # map[0, 4] = 2
+    # map[0, 5] = 2
+    # map[0, 6] = 2
+    # map[0, 7] = 2
+    # map[0, 8] = 2
+    # map[0, 9] = 2
+
     map = np.zeros(shape=(50, 50))
-    map[:, 4] = 1
-    map[:, 5] = 1
-    map[49, 4] = 3
-    map[49, 5] = 3
-    map[0, 4] = 2
-    map[0, 5] = 2
+    map[:35, 24] = 1
+    map[:35, 25] = 1
+    map[:35, 26] = 1
+    map[:35, 27] = 1
+    map[:35, 28] = 1
+    map[:35, 29] = 1
+
+    map[29, 1:24] = 1
+    map[30, 1:24] = 1
+    map[31, 1:24] = 1
+    map[32, 1:24] = 1
+    map[33, 1:24] = 1
+    map[34, 1:24] = 1
+
+    map[29, 0] = 3
+    map[30, 0] = 3
+    map[31, 0] = 3
+    map[32, 0] = 3
+    map[33, 0] = 3
+    map[34, 0] = 3
+
+    map[0, 24] = 2
+    map[0, 25] = 2
+    map[0, 26] = 2
+    map[0, 27] = 2
+    map[0, 28] = 2
+    map[0, 29] = 2
 
     game = Game(racetrack=map, visualize=False)
     model = ModelRLMC()
 
-    for _ in range(0, 100):
+    for i in range(0, 3000):
         episode: list[tuple[State, Action, int]] = []
         n_steps = 0
         while not game.is_finished() and n_steps < 1000:
@@ -48,6 +110,8 @@ def train_ai():
             reward = game.step(action)
             episode.append((state,action,reward))
             n_steps += 1
+        if i % 500 == 0:
+            print(str(n_steps) + " " + str(i))
         model.learn(episode)
         game.reset()
 
@@ -61,12 +125,13 @@ def train_ai():
         print(f"ai plays step {n_steps}")
         state = game.get_state()
         action = model.determine_action(state)
-        print(f"action: {action}")
         game.step(action)
+        print(f"action: {action}, pos: {game.get_state().agent_position}, vel: {game.get_state().agent_velocity}")
         n_steps += 1
-        time.sleep(2)
+        time.sleep(1)
 
 
 
 
 train_ai()
+# play_user()
