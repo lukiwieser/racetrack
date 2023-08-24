333f12554bf422d40cb967a8304ae564a9fa154b
Lukas Wieser <lukas.luki.wieser@gmail.com>
2023-08-24 19:45:31 +0200

    Add missing typehints to EpisodeVisualizer

--- classes/episode_visualizer.py
+++ classes/episode_visualizer.py
@@ -2,6 +2,12 @@ import copy
 import threading
 from functools import partial
 from tkinter import *
+from typing import Any
+
+import numpy as np
+
+from classes.action import Action
+from classes.state import State
 
 
 class EpisodeVisualizer:
@@ -10,22 +16,22 @@ class EpisodeVisualizer:
     The game is given by a single episode.
     """
 
-    def visualize_episode(self, map, episode, title):
-        t = threading.Thread(target=partial(self._run_in_thread, map, episode, title))
+    def visualize_episode(self, track: np.ndarray, episode: list[tuple[State, Action, int]], title: str) -> None:
+        t = threading.Thread(target=partial(self._run_in_thread, track, episode, title))
         t.start()
 
-    def _run_in_thread(self, map, episode, title):
-        vis = EpisodeVisualizerIntern(map, episode, title)
+    def _run_in_thread(self, track: np.ndarray, episode, title: str) -> None:
+        EpisodeVisualizerIntern(track, episode, title)
 
 class EpisodeVisualizerIntern:
-    def __init__(self, map, episode, title, boardsize=600):
+    def __init__(self, map: np.ndarray, episode: list[tuple[State, Action, int]], title: str, boardsize: int = 600):
         self.map = copy.deepcopy(map)
         self.episode = copy.deepcopy(episode)
 
         # initiate the gameboard
         self.init_board(boardsize, title)
 
-    def init_board(self, boardsize, title):
+    def init_board(self, boardsize: int, title: str):
         """
         Creates the tkinter window and initializes it with the correct state.
 
@@ -39,18 +45,18 @@ class EpisodeVisualizerIntern:
         # create grid of rectangles and safe it
         self.board = self.create_board(copy.deepcopy(self.map), boardsize)
 
-        # draw epsiode
+        # draw episode
         self.draw_episode()
 
         # start blocking main loop
         self.window.mainloop()
 
-    def draw_episode(self):
+    def draw_episode(self) -> None:
         for t in self.episode:
             pos = t[0].agent_position
             self.change_color(self.board[pos[0]][pos[1]])
 
-    def change_color(self, item):
+    def change_color(self, item: Any) -> None:
         """
         Changes the color of a specific rectangle in the grid.
 
@@ -58,7 +64,7 @@ class EpisodeVisualizerIntern:
         """
         self.canvas.itemconfig(int(item), fill="red")
 
-    def create_board(self, input_array, boardsize):
+    def create_board(self, input_array: np.ndarray, boardsize: int) -> np.ndarray:
         """
         Converts the initial ndarray-2d array into a grid of rectangles.
         Assumption: the input array has the structure NxN
@@ -77,7 +83,7 @@ class EpisodeVisualizerIntern:
                 input_array[i][j] = self.canvas.create_rectangle(x1, y1, x2, y2, fill=color)
         return input_array
 
-    def get_color(self, num: int):
+    def get_color(self, num: int) -> str:
         """
         Returns color corresponding to the cell type.
 
