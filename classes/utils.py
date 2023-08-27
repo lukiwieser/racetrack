49c8aade1df2c76c475e8432644c4f6cc984efb7
Lukas Wieser <lukas.luki.wieser@gmail.com>
2023-08-27 23:19:22 +0200

    Add docstrings & missing typehints to check_positive_int & get_track

--- classes/utils.py
+++ classes/utils.py
@@ -6,7 +6,11 @@ from classes.generator import Generator
 from classes.racetrack_list import RacetrackList
 
 
-def check_positive_int(value_str: str):
+def check_positive_int(value_str: str) -> int:
+    """
+    Helper function for argparse, to convert arguments into positive integers
+    @param value_str: argument value
+    """
     try:
         value = int(value_str)
     except ValueError:
@@ -17,6 +21,13 @@ def check_positive_int(value_str: str):
 
 
 def get_track(track_number: int | None, track_random_seed: int | None) -> np.ndarray:
+    """
+    Get a racetrack from either the predefined tracklist, or the racetrack generator.
+    The arguments track_number & track_random_seed are mutually exclusive.
+
+    :param track_number: Number of the racetrack on the predefined tracklist
+    :param track_random_seed: seed for the racetrack generator
+    """
     if track_number is not None:
         return RacetrackList.get_track(track_number)
     if track_random_seed is not None:
